402363378e83365ebaa9a07c0f146f76c4e41c74
Sameer Danish <sameerdanishlohano@gmail.com>
2026-02-04 23:38:07 +0500

    Added face detection functionality in face_detection.py
    Implement face detection using YuNet model with OpenCV.

--- src/face_detection.py
+++ src/face_detection.py
@@ -1,8 +1,22 @@
-"""
-face_detection.py
+import cv2
 
-Handles face detection using MediaPipe.
-"""
+model_path="models/face_detection_yunet_2023mar.onnx"
+img=cv2.imread("data/raw_faces/hadeer.png")
 
-def detect_face(image):
-    pass
+if img is None:
+    raise ValueError("Image not found!")
+
+h,w,_=img.shape
+
+detector=cv2.FaceDetectorYN.create(model_path,"",(w,h),score_threshold=0.9,nms_threshold=0.3,top_k=5000)
+_,faces=detector.detect(img)
+
+if faces is not None:
+    for face in faces:
+        x,y,fw,fh=map(int,face[:4])
+        cv2.rectangle(img,(x,y),(x+fw,y+fh),(0,255,0),2)
+
+cv2.imshow("Face Detection - YuNet",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
